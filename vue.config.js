f01ad3ed749d0a1a0c7fc44ba5f8808f839fec26
jcsou <jcsouvignet@gmail.com>
2023-12-17 19:54:40 +0100

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/fcvlfutsal_u13f_u15f_2024/dist/'
+    : '/',
+  transpileDependencies: [
+    'vuetify'
+  ]
 })
